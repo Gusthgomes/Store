17b55d8fafe468c62a719345e979a588116b9657
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-10-16 20:58:27 -0300

    style: add background gradient

--- components/Catalog-item.tsx
+++ components/Catalog-item.tsx
@@ -10,7 +10,7 @@ const CatalogItem = ({ category }: CategoryItemProps) => {
   return (
     <Link href={`/category/${category.slug}`}>
       <div className="flex flex-col">
-        <div className="flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-category-item-gradient">
+        <div className="to-[rgba(80, 51, 195)] flex h-[150px] w-full items-center justify-center rounded-tl-lg rounded-tr-lg bg-gradient-to-r from-[#5033c3]">
           <Image
             src={category.imageUrl}
             alt={category.name}
