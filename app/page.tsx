282ab455b9f4fc282bd27b3c5dc7eb4ed324e34c
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-10-15 22:00:58 -0300

    feat: add Product components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Link from "next/link";
 
+import { db } from "@/lib/prisma";
+
 import PromoBanner from "@/components/Promo-banner";
 import CategoryList from "@/components/Category-list";
+import ProductList from "@/components/ProductList";
 
-export default function Home() {
+export default async function Home() {
+  const deals = await db.product.findMany({
+    where: {
+      discountPercentage: {
+        gt: 0,
+      },
+    },
+  });
   return (
     <>
       <div className="mx-auto max-w-[1920px]">
@@ -14,6 +24,10 @@ export default function Home() {
 
       <div className="px-5 py-6 lg:mt-2">
         <CategoryList />
+      </div>
+
+      <div className="flex flex-col gap-3 lg:gap-5">
+        <ProductList products={deals} />
       </div>
     </>
   );
