89c5634492e53d30826f3ee21dfed434037e0ae8
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-10-15 21:28:07 -0300

    feat: initial product item component

---
+++ components/Product-item.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+import { ProductWithTotalPrice } from "@/helpers/products";
+
+interface ProductItemProps {
+  product: ProductWithTotalPrice;
+  className?: string;
+}
+
+const ProductItem = ({ product, className }: ProductItemProps) => {
+  return (
+    <Link
+      href={`/product/${product.slug}`}
+      className={cn("flex min-w-[156px] flex-col gap-4", className)}
+    >
+      <div className="relative flex aspect-square w-full items-center justify-center rounded-lg bg-accent">
+        <Image
+          src={product.imageUrls[0]}
+          height={0}
+          width={0}
+          sizes="100vw"
+          className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
+          alt={product.name}
+        />
+      </div>
+    </Link>
+  );
+};
+
+export default ProductItem;
