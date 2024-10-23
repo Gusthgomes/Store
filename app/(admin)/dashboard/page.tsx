3bdf6919a629c1c96a0bc726d77df31698913877
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-10-23 20:28:59 -0300

    feat: add admin dashboard

---
+++ app/(admin)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Card } from "@/components/ui/card";
+
+const DashboardPage = () => {
+  return (
+    <Card className="flex items-center justify-center gap-3 mx-auto">
+      Dashboard administrativo
+    </Card>
+  );
+};
+
+export default DashboardPage;
