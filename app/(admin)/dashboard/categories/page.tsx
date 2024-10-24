5f136075050907b362252ad50ae3e8d4d689d83c
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-10-24 20:20:05 -0300

    feat: add admin categories page

---
+++ app/(admin)/dashboard/categories/page.tsx
@@ -0,0 +1,41 @@
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/prisma";
+import { ListOrderedIcon, PlusIcon } from "lucide-react";
+import CategoriesTable from "../components/Categories-table";
+
+const CategoriesPage = async () => {
+  const categories = await db.category.findMany({
+    include: {
+      products: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  });
+
+  return (
+    <div className="flex w-full flex-col gap-10 p-10">
+      <Badge variant="heading">
+        <ListOrderedIcon size={18} />
+        Categorias
+      </Badge>
+
+      <div className="flex w-full items-center justify-between">
+        <p className="text-lg font-bold">
+          Categorias encontradas: {categories.length}
+        </p>
+
+        <Button className="flex gap-2">
+          <PlusIcon size={18} />
+          Adicionar categoria
+        </Button>
+      </div>
+
+      <CategoriesTable categories={categories} />
+    </div>
+  );
+};
+
+export default CategoriesPage;
